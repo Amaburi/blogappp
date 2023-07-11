a6b2c10057de9b90290c591c853e498ebabf563a
Amaburi <drsservant@gmail.com>
2023-07-11 14:54:45 +0700

    Comment Form Done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import FeaturedPosts from '@/sections/FeaturedPost';
 import { PostCard, PostWidget, Categories } from '@/components/indes';
 import { getPosts } from '../services';
-
+import Head from 'next/head';
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Powered By GraphCMS</title>
+      </Head>
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
